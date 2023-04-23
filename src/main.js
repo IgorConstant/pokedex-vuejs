724cac895352416bb264882c5497725e3e420cf5
Igor <igorhenriqueconstant@gmail.com>
2023-04-23 16:08:50 -0300

    Ajustes na HomeView para consumir a API de Pokémons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//CSS
+import './assets/css/base.css'
+import './assets/css/style.css'
+
 createApp(App).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="mx-auto px-8 mt-6 mb-6">
+      <div class="grid grid-cols-6 gap-4">
+        <div
+          :class="['p-4', 'rounded', 'shadow-lg', 'cardBody']"
+          :style="{ background: pokemon.typeColor }"
+          v-for="pokemon in pokemons"
+          :key="pokemon.id"
+        >
+          <img :class="['float-right']" :src="pokemon.imageUrl" :alt="pokemon.name" />
+          <h2 :class="['cardheadertitle', 'uppercase']">{{ pokemon.name }}</h2>
+          <p :class="['mt-4']">
+            tipo: <span :class="['badge']">{{ pokemon.type }}</span>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  data() {
+    return {
+      pokemons: [],
+      typeColors: {
+        water: "#6390f0",
+        fire: "#ee8130",
+        grass: "#7ac74c",
+        bug: "#a6b91a",
+        electric: "#f7d02c",
+        psychic: "#f95587",
+        poison: "#6f35fc",
+        flying: "#a98ff3",
+        normal: "#a8a77a",
+        ground: "#e2bf65",
+        rock: "#b6a136",
+        ghost: "#735797",
+        dragon: "#1575ff",
+        fairy: "#d685ad",
+        fighting: "#c22e28",
+        ice: "#96d9d6",
+      },
+    };
+  },
+  created() {
+    this.getPokemons();
+  },
+  methods: {
+    async getPokemons() {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=150"
+        );
+        const pokemonsData = await Promise.all(
+          response.data.results.map(async (pokemon) => {
+            const pokemonDetail = await axios.get(pokemon.url);
+            return {
+              name: pokemonDetail.data.name,
+              type: pokemonDetail.data.types.map((t) => t.type.name).join(", "),
+              id: pokemonDetail.data.id,
+              imageUrl: pokemonDetail.data.sprites.front_default,
+              typeColor: this.typeColors[pokemonDetail.data.types[0].type.name],
+            };
+          })
+        );
+        this.pokemons = pokemonsData;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
